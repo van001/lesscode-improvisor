b9eab3cc060e593b71a531aac5ab83fbaa58118e
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-05-20 20:17:38 -0700

    error loggin only on staucode > 500

--- executors.js
+++ executors.js
@@ -1,9 +1,9 @@
-const { HttpPOST, ExtractData, $M, lappend, Hint } = require('lesscode-fp')
+const { HttpPOST, ExtractData, $M, lappend } = require('lesscode-fp')
 const { S3Put } = require('./aws')
 const Result = data => async output => ({ res: lappend(data.res)([output]), data: output })
 
 module.exports = {
     // structure { res , data}
-    'http-post': workflow => async data => $M(Result(data), ExtractData, HttpPOST({ data: data.data }), Hint(`${JSON.stringify(data)}`))(workflow.executor.url),
+    'http-post': workflow => async data => $M(Result(data), ExtractData, HttpPOST({ data: data.data }))(workflow.executor.url),
     's3-put': workflow => async data => $M(Result(data), S3Put({ Bucket: workflow.executor.Bucket, Key: workflow.executor.Key }))(JSON.stringify(data.data))
 }
--- improvisorv1.js
+++ improvisorv1.js
@@ -1,8 +1,8 @@
-const { $, $M, lreverse, lmap, lhead } = require('lesscode-fp')
+const { $, $M, lreverse, lmap, lhead, } = require('lesscode-fp')
 const Executors = require('./executors')
 
 // Error Handler
-const Error = msg => async err =>  { Workflows(lreverse(msg.error.workflows))({ res: [], data: err }); throw { StatusCode: err.statusCode, err : JSON.stringify(err) }}
+const Error = msg => async err =>  { if (err.statusCode > 500) { Workflows(lreverse(msg.error.workflows))({ res: [], data: err })}; throw { StatusCode: err.statusCode, err : JSON.stringify(err) }}
 
 // Result handler
 const Result = msg => async data => Workflows(lreverse(msg.result.workflows))({ res: [], data: data.res })
