c443139478849862d46417d37512a2c047007132
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-06-16 15:54:08 -0700

    added excecutor - logger

--- executors.js
+++ executors.js
@@ -1,9 +1,10 @@
-const { HttpPOST, ExtractData, $M, lappend } = require('lesscode-fp')
+const { HttpPOST, ExtractData, $M, lappend, Print } = require('lesscode-fp')
 const { S3Put } = require('./aws')
 const Result = data => async output => ({ res: lappend(data.res)([output]), data: output })
 
 module.exports = {
     // structure { res , data}
-    'http-post': workflow => async data => $M(Result(data), ExtractData, HttpPOST({ data: data.data }))(workflow.executor.url),
+    'logger' : workflow => async data => $M(Result(data),Print)(JSON.stringify({ type : 'info', ...data.res[data.res.length-1]})),
+    'http-post': workflow => async data => $M(Result(data), ExtractData, HttpPOST({ data: data.data }), Print)(workflow.executor.url),
     's3-put': workflow => async data => $M(Result(data), S3Put({ Bucket: workflow.executor.Bucket, Key: workflow.executor.Key }))(JSON.stringify(data.data))
 }
